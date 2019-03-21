cd524084849e5fd959fd5e367afd590242ed6ed0
Keith <ozmaneku@gmail.com>
2019-03-21 11:29:15 -0400

    Adding in Heroku capabilities and Client pathing

--- server.js
+++ server.js
@@ -7,8 +7,12 @@ const villagerController = require('./controllers/villagerController')
 app.use(logger('dev'))
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(express.static(`${__dirname}/client/build`))
 
 app.use('/', router)
+app.get('/*', (req, res) => {
+    res.sendFile(`${__dirname}/client/build/index.html`)
+  })
 
 const PORT = process.env.PORT || 3000
 
